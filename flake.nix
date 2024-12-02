a9a766a5b14ceb063305fe2045ab4c9a613c7908
wchresta <34962284+wchresta@users.noreply.github.com>
2024-12-02 20:51:43 +0100

    Init flake

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{ outputs = { nixpkgs, flake-utils, ... }:
+  flake-utils.lib.eachDefaultSystem(system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        cargo
+        rustc
+        rustfmt
+      ];
+    };
+  });
+}
