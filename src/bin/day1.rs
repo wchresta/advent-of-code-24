424aa544a99785c9413198af65df2aec2d73a34c
wchresta <34962284+wchresta@users.noreply.github.com>
2024-12-02 21:50:53 +0100

    Solve day1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,10 +1,44 @@
+use std::{collections::HashMap, iter::zip};
+
 use advent_of_code_24::input;
 
 fn main() {
     println!("Hello, world!");
     let content: Vec<[i64;2]> = input::read_i64("day1");
+    part1(&content);
+    part2(&content);
+}
 
+fn part1(content: &Vec<[i64;2]>) {
+    let mut lefts = Vec::new();
+    let mut rights = Vec::new();
     for [left, right] in content {
-        println!("left: {}, right: {}", left, right);
+        lefts.push(left);
+        rights.push(right);
     }
+
+    lefts.sort();
+    rights.sort();
+
+    println!("part1: {}", zip(lefts, rights).map(|(x,y)| (y-x).abs()).sum::<i64>())
+}
+
+fn part2(content: &Vec<[i64;2]>) {
+    let mut right_counts = HashMap::<i64,i64>::new();
+    for [_, right] in content {
+        match right_counts.get_mut(&right) {
+            Some(x) => {*x += 1;},
+            None => {right_counts.insert(*right, 1);},
+        }
+    }
+
+    let mut score = 0;
+    for [left, _] in content {
+        match right_counts.get(&left) {
+            Some(count) => score += left * count,
+            _ => {},
+        }
+    }
+
+    println!("part2: {}", score)
 }
