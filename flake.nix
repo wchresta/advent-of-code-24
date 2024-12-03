aa09c29d1cf50d3042961285be7da5b159b75c24
wchresta <34962284+wchresta@users.noreply.github.com>
2024-12-03 09:00:31 +0100

    Fix rust-analyzer

--- flake.nix
+++ flake.nix
@@ -8,7 +8,13 @@
         cargo
         rustc
         rustfmt
+        rust-analyzer
       ];
+
+      # Certain Rust tools won't work without this
+      # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
+      # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
+      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
     };
   });
 }
