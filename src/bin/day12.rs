0238e0d6b01771e6b3197ace1cc6fc9b969507bc
wchresta <34962284+wchresta@users.noreply.github.com>
2024-12-12 22:48:37 +0100

    Solve day12 part1

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::{HashMap, HashSet},
-    hash::Hash,
-};
+use std::collections::HashSet;
 
 use advent_of_code_24::{input, solve, square, test1};
 
@@ -11,7 +8,10 @@ fn main() {
     solve("day12", input::as_vecvec, part1, part2);
 }
 
-fn part1(garden: &Vec<Vec<char>>) -> u64 {}
+fn part1(garden: &Vec<Vec<char>>) -> u64 {
+    let regions = partition(garden);
+    regions.iter().map(score).sum()
+}
 
 fn partition(garden: &Vec<Vec<char>>) -> Vec<Vec<Pos>> {
     let mut regions = Vec::new();
@@ -19,7 +19,16 @@ fn partition(garden: &Vec<Vec<char>>) -> Vec<Vec<Pos>> {
 
     for y in 0..garden.len() {
         for x in 0..garden[y].len() {
-            let t = garden[y][x];
+            if seen.contains(&(x, y)) {
+                continue;
+            }
+
+            seen.insert((x, y));
+            let region = explore_region(garden, (x, y));
+            for r in &region {
+                seen.insert(r.to_owned());
+            }
+            regions.push(region);
         }
     }
     regions
@@ -28,19 +37,31 @@ fn partition(garden: &Vec<Vec<char>>) -> Vec<Vec<Pos>> {
 fn explore_region(garden: &Vec<Vec<char>>, (x, y): Pos) -> Vec<Pos> {
     let r = garden[y][x];
     let mut stack = Vec::from([(x, y)]);
-    let mut seen = HashSet::new();
-
-    let mut found = Vec::new();
+    let mut seen = HashSet::from([(x, y)]);
     while let Some(pos) = stack.pop() {
         square::straight_neighbours(garden, pos, |_, v| *v == r).for_each(|(p, _)| {
-            found.push(p.to_owned());
             if !seen.contains(&p) {
                 stack.push(p.to_owned());
                 seen.insert(p);
             }
         })
     }
-    found
+    seen.into_iter().collect()
+}
+
+fn score(region: &Vec<Pos>) -> u64 {
+    let h: HashSet<(isize, isize)> =
+        HashSet::from_iter(region.iter().map(|(x, y)| (*x as isize, *y as isize)));
+    let mut area = 0;
+    let mut perimeter = 0;
+    for (x, y) in region {
+        area += 1;
+        perimeter += [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            .iter()
+            .filter(|(dx, dy)| !h.contains(&(*x as isize + dx, *y as isize + dy)))
+            .count();
+    }
+    (area * perimeter) as u64
 }
 
 fn part2(stones: &Vec<Vec<char>>) -> u64 {
@@ -61,5 +82,5 @@ MMMISSJEEE";
 
 #[test]
 fn test_part1() {
-    test1("125 17", 1930, input::as_vecvec, part1);
+    test1(TEST_INPUT, 1930, input::as_vecvec, part1);
 }
--- src/square.rs
+++ src/square.rs
@@ -8,19 +8,31 @@ pub fn straight_neighbours<'a, T>(
 where
     T: Sized + 'a,
 {
+    straight_neighbour_pos(square, (x, y)).filter_map(move |(x, y)| {
+        let v = &square[y][x];
+        if is_neighbour((x, y), v) {
+            Some(((x, y), v))
+        } else {
+            None
+        }
+    })
+}
+
+pub fn straight_neighbour_pos<'a, T>(
+    square: &'a Vec<Vec<T>>,
+    (x, y): Pos,
+) -> impl Iterator<Item = Pos> + 'a {
     [(1, 0), (-1, 0), (0, 1), (0, -1)]
         .iter()
         .filter_map(move |(dx, dy)| {
-            let (x2, y2) = (x as isize + dx, y as isize + dy);
-            if x2.is_negative() || y2.is_negative() {
+            if x == 0 && *dx < 0 || y == 0 && *dy < 0 {
                 return None;
             }
-            let pos = (x2 as usize, y2 as usize);
-            let v = square.get(pos.1)?.get(pos.0)?;
-            if is_neighbour(pos, v) {
-                Some((pos, v))
-            } else {
-                None
+            if (x as isize + dx) as usize >= square.len()
+                || (y as isize + dy) as usize >= square[0].len()
+            {
+                return None;
             }
+            return Some(((x as isize + dx) as usize, (y as isize + dy) as usize));
         })
 }
